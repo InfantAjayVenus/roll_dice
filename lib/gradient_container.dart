f7fe9cc9ccb7f20665acf1b75aae3fa28d94e5f4
ajay <infantcsajay@outlook.com>
2023-04-14 08:47:00 +0530

    [container] feat: add default gradient constructor

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -3,9 +3,18 @@ import 'package:roll_dice/dice_roller.dart';
 
 const startAlignment = Alignment.bottomRight;
 const endAlignment = Alignment.topLeft;
+const List<Color> sunsetGradientColors = [
+  Colors.deepPurple,
+  Colors.deepPurpleAccent,
+  Colors.blueAccent,
+  Colors.blue,
+];
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
+
+  const GradientContainer.sunset({super.key}) : colors = sunsetGradientColors;
+
   final List<Color> colors;
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice/gradient_container.dart';
 
-const List<Color> gradientColors = [
-  Colors.deepPurple,
-  Colors.deepPurpleAccent,
-  Colors.blueAccent,
-  Colors.blue,
-];
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
       backgroundColor: Color.fromARGB(178, 97, 63, 177),
-      body: GradientContainer(colors: gradientColors),
+      body: GradientContainer.sunset(),
     ),
   ));
 }
